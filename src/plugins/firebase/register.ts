bfd398d7389f0beeccd55428414bb78c2b2a27f0
Ryoya Tamura <lollipop@simochee.net>
2024-03-15 19:05:09 +0900

    🚧 remove inject

--- src/plugins/firebase/register.ts
+++ src/plugins/firebase/register.ts
@@ -21,17 +21,17 @@ export default (context: Context, inject: Function) => {
     NUXT_ENV_MESSAGING_SENDER_ID,
   } = env;
 
-  inject(
-    'firebase',
-    new Firebase({
-      apiKey: NUXT_ENV_API_KEY,
-      authDomain: NUXT_ENV_AUTH_DOMAIN,
-      datebaseUrl: NUXT_ENV_DATABASE_URL,
-      messagingSenderId: NUXT_ENV_MESSAGING_SENDER_ID,
-      projectId: NUXT_ENV_PROJECT_ID,
-      storageBucket: NUXT_ENV_STORAGE_BUCKET,
-    }),
-  );
+  // inject(
+  //   'firebase',
+  //   new Firebase({
+  //     apiKey: NUXT_ENV_API_KEY,
+  //     authDomain: NUXT_ENV_AUTH_DOMAIN,
+  //     datebaseUrl: NUXT_ENV_DATABASE_URL,
+  //     messagingSenderId: NUXT_ENV_MESSAGING_SENDER_ID,
+  //     projectId: NUXT_ENV_PROJECT_ID,
+  //     storageBucket: NUXT_ENV_STORAGE_BUCKET,
+  //   }),
+  // );
 
   Firebase.listeners.onAuthStateChanged.push(
     async (loggedIn: boolean, userId?: string, uid?: string) => {
